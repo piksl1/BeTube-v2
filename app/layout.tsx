36154466d4e0a63feeefebd0a298fa7c68ad95e1
Wassim Zaoui <75567675+piksl1@users.noreply.github.com>
2025-04-20 22:15:36 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,7 +11,6 @@ const inter = Inter({ subsets: ["latin"] })
 export const metadata: Metadata = {
   title: "beTube - YouTube Clone",
   description: "A YouTube clone built with Next.js and TypeScript",
-    generator: 'v0.dev'
 }
 
 export default function RootLayout({
